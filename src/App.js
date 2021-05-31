784cb5b0df3b9e608f6a2343515a8c1796bceafb
Kevin Ko <clickingmouse@hotmail.com>
2021-05-31 16:42:19 +0800

    initial amplify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
